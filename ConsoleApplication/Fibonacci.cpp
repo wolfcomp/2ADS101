f06e96ac045cd6b81afa15b2588cb44a62e2e426
wolfcomp <magnus1997.mho@gmail.com>
2023-08-21 09:06:00 +0200

    correct console logs

--- ConsoleApplication/Fibonacci.cpp
+++ ConsoleApplication/Fibonacci.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 
-static int n1 = 0, n2 = 1, n3;
+static int n1 = 0, n2 = 1;
 
 void fibonacci_main()
 {
@@ -11,15 +11,23 @@ void fibonacci_main()
 	n2 = 1;
 	std::cout << "Enter a how many positions to find Fibonacci sequence for: ";
 	std::cin >> n;
-	std::cout << n1 << " " << n2 << " ";
-	fibonacci(0, n - 2);
+	if (n < 1)
+	{
+		throw std::invalid_argument("n must be greater than or equal to 1");
+	}
+	if (n == 1)
+		std::cout << n1 << " ";
+	if (n >= 2)
+		std::cout << n1 << " " << n2 << " ";
+	if (n > 2)
+		fibonacci(0, n - 2);
 }
 
 void fibonacci(int count, int limit)
 {
 	if (count < limit)
 	{
-		n3 = n1 + n2;
+		const int n3 = n1 + n2;
 		n1 = n2;
 		n2 = n3;
 		std::cout << n3 << " ";
